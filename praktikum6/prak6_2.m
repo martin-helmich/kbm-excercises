369047c190eb1cc64a1f59c8104f580db7c5b271
Martin Helmich <martin.helmich@hs-osnabrueck.de>
2013-06-04 16:09:43 +0200

    Praktikum 6, Aufg. 2

--- praktikum6/prak6_2.m
+++ praktikum6/prak6_2.m
@@ -1,3 +1,12 @@
+n_in  = 2;
+n_middle1 = 2;
+n_middle2 = 2;
+n_out = 4;
+
+W1 = 2 * rand(n_middle1, n_in + 1) - 1;
+W2 = 2 * rand(n_middle2, n_middle1 + 1) - 1;
+W3 = 2 * rand(n_out , n_middle2 + 1) - 1;
+
 n=100;
 Mx=10;
 E=Mx*rand([2,n])-Mx/2*ones(2,n); % Mittelwert Null
@@ -8,7 +17,9 @@ and(not(k1),k2); ... % 2. Quadrant
 and(not(k1),not(k2)); ... % 3. Quadrant
 and(k1,not(k2))]; % 4. Quadrant
 
-plot(E(1,A(1,:)),E(2,A(1,:)),'ok'); hold on
-plot(E(1,A(2,:)),E(2,A(2,:)),'*k');
-plot(E(1,A(3,:)),E(2,A(3,:)),'sk');
-plot(E(1,A(4,:)),E(2,A(4,:)),'pk');
+%plot(E(1,A(1,:)),E(2,A(1,:)),'ok'); hold on
+%plot(E(1,A(2,:)),E(2,A(2,:)),'*k');
+%plot(E(1,A(3,:)),E(2,A(3,:)),'sk');
+%plot(E(1,A(4,:)),E(2,A(4,:)),'pk');
+
+[W1,W2,W3] = train2(W1,W2,W3,E(:,1:50),A);
---
+++ praktikum6/train2.m
@@ -0,0 +1,68 @@
+function [ W1,W2,W3 ] = train2( W1,W2,W3,E,A )
+%TRAIN Summary of this function goes here
+%   Detailed explanation goes here
+
+columns = size(E, 2);
+sigma   = 0.1;
+sse0    = 1000;
+sse     = 0;
+
+fehler = []
+
+for j = 1:5000
+    sse = 0;
+    g1 = zeros(size(W1));
+    g2 = zeros(size(W2));
+    g3 = zeros(size(W3));
+
+    for i = 1:columns
+       e = E(:,i);
+       a = A(:,i);
+
+       [d, e1, e2, e3, d1, d2] = values2(W1, W2, W3, e);
+       %sse = sse + (d - a)^2;
+       sse = sse + sum((d - a).^2);
+
+       delta3 = 2 * jacobi(e3) * (d - a);
+       delta2 =     jacobi(e2) * W3(:,1:end-1)' * delta3;
+       delta1 =     jacobi(e1) * W2(:,1:end-1)' * delta2;
+
+       gradient1 = delta1 * [e;1]';
+       gradient2 = delta2 * d1';
+       gradient3 = delta3 * d2';
+
+       g1 = g1 + gradient1;
+       g2 = g2 + gradient2;
+       g3 = g3 + gradient3;
+    end
+
+    %disp(g1);
+    %disp(g2);
+
+    W1 = W1 - sigma * g1;
+    W2 = W2 - sigma * g2;
+    W3 = W3 - sigma * g3;
+    
+    if (sse > sse0)
+        sigma = sigma * 0.5;
+        fprintf('Hoppla\n', sigma);
+    else
+        sigma = sigma * 1.01;
+    end
+    
+    sse0 = sse;
+
+    fehler = [fehler ; sse ];
+    fprintf('Durchschnitts-Fehler nach %i Iterationen: %f (sigm: %f)\n', j, sse, sigma);
+    
+    if sse < 0.001
+        fprintf('Reicht jetzt.\n');
+        return
+    end
+end
+
+figure
+plot(fehler);
+
+end
+
